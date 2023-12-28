2a2e5836f44fff33c3a24e7b603368dda49b3ca0
progamerxml <progamerxml@outlook.com>
2023-12-28 16:07:12 +0700

    Buat test simpan sukses GrupTeleRepositoryTest

--- tests/Repository/GrupTeleRepositoryTest.php
+++ tests/Repository/GrupTeleRepositoryTest.php
@@ -5,8 +5,26 @@ declare(strict_types=1);
 namespace PRGANYRN\PROJECT\TEST\Repository;
 
 use PHPUnit\Framework\TestCase;
+use PRGANYRN\PROJECT\TEST\Config\Database;
+use PRGANYRN\PROJECT\TEST\Domain\Grup;
 
 class GrupTeleRepositoryTest extends TestCase
 {
-    
+    private GrupTeleRepository $grupRepo;
+
+    protected function setUp(): void
+    {
+        $this->grupRepo = new GrupTeleRepository(Database::getConnection());
+
+        $this->grupRepo->hapusAll();
+    }
+    public function testSimpanSukses()
+    {
+        $grup = new Grup();
+        $grup->id_grup = '079856412';
+        $grup->nama_grup = 'Grup Test 1';
+        $grup->user_grup = 'grup_test1';
+
+        $this->grupRepo->simpan($grup);
+    }
 }
